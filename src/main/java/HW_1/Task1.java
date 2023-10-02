11a8ae3916067c44c85be2f028a68463e5a7277b
Oksana <oksana.syrotiyk@pushgaming.com>
2023-10-02 18:09:35 +0100

    HW_Task1

---
+++ src/main/java/HW_1/Task1.java
@@ -0,0 +1,23 @@
+package HW_1;
+
+public class Task1 {
+    /**
+     *
+     * starting point for program execution
+     */
+    public static void main(String[] args) {
+        String name = "Oksana";
+        String city = "Lviv";
+        int age = 30;
+        String university = "Lviv Polytechnic National University";
+        String pet = "cat";
+        String petName = "Martin";
+        String hobby = "travelling";
+        String professional = "junior software developer";
+
+        System.out.println(
+                "My name is " + name + ". I was born and raised in " + city + ". I’m in my early " + age +
+                        ". Studied chemistry at \n" + university + ". I have, " + pet + " his name is " + petName + ". I am very sociable, so I have many friends. \nI have a large and friendly family, with whom I also like to spend my free time. My hobby is " + hobby +". \nNow I'm working in an IT company as a " + professional
+                          + ". \nI start this course to improve my knowledge and get some more information");
+    }
+}
