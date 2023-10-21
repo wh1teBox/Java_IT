3ddb7132f52266c04b62bebbf69c8711f0e1e056
Oksana <oksana.syrotiyk@pushgaming.com>
2023-10-21 12:04:53 +0100

    HW task4

---
+++ src/main/java/HW/Task4/Task1.java
@@ -0,0 +1,30 @@
+package HW.Task4;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        /* 1) Користувач вводить з клавіатури деякі числа (a, b, c), вивести на екран всі числа які є одночасно кратними числам (a, b, c) з діапазону від 0 до 100000*/
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the value of a: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Enter the value of b: ");
+        int b = scanner.nextInt();
+
+        System.out.print("Enter the value of c: ");
+        int c = scanner.nextInt();
+
+        if (a == 0 || b == 0 || c == 0) {
+            System.out.println("a, b, and c must be non-zero values.");
+        } else {
+            for (int number = 0; number <= 100000; number++) {
+                if (number % a == 0 && number % b == 0 && number % c == 0) {
+                    System.out.println(number);
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/HW/Task4/Task3.java
@@ -0,0 +1,72 @@
+package HW.Task4;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task3 {
+    /*Реалізуйте серію з n ігор "Камінь, ножиці, папір" з комп'ютером.
+     В результаті виведіть статистику: скільки ігор виграв користувач, скільки разів кожного виду ходів було вибрано.
+     Доповніть гру аналізом комп'ютера ваших ходів і вибір найбільш підходящого проти вас ходу.*/
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            Random random = new Random();
+
+            int userWins = 0;
+            int computerWins = 0;
+            int ties = 0;
+            int[] userChoices = new int[3]; // Індекси: 0 - камінь, 1 - ножиці, 2 - папір
+
+            String[] choices = {"Камінь", "Ножиці", "Папір"};
+
+            System.out.println("Ласкаво просимо до гри 'Камінь, ножиці, папір'!");
+
+            while (true) {
+                System.out.print("Виберіть свій хід (0 - Камінь, 1 - Ножиці, 2 - Папір, -1 - Закінчити гру): ");
+                int userChoice = scanner.nextInt();
+
+                if (userChoice == -1) {
+                    break;
+                }
+
+                if (userChoice < 0 || userChoice > 2) {
+                    System.out.println("Некоректний вибір. Будь ласка, виберіть ще раз.");
+                    continue;
+                }
+
+                int computerChoice = random.nextInt(3);
+
+                System.out.println("Ваш хід: " + choices[userChoice]);
+                System.out.println("Хід комп'ютера: " + choices[computerChoice]);
+
+                if (userChoice == computerChoice) {
+                    System.out.println("Нічия!");
+                    ties++;
+                } else if ((userChoice == 0 && computerChoice == 1) ||
+                        (userChoice == 1 && computerChoice == 2) ||
+                        (userChoice == 2 && computerChoice == 0)) {
+                    System.out.println("Ви виграли!");
+                    userWins++;
+                } else {
+                    System.out.println("Комп'ютер виграв!");
+                    computerWins++;
+                }
+
+                userChoices[userChoice]++;
+            }
+
+            System.out.println("Статистика:");
+            System.out.println("Кількість перемог гравця: " + userWins);
+            System.out.println("Кількість перемог комп'ютера: " + computerWins);
+            System.out.println("Кількість нічий: " + ties);
+
+            int mostFrequentUserChoice = 0;
+            for (int i = 1; i < 3; i++) {
+                if (userChoices[i] > userChoices[mostFrequentUserChoice]) {
+                    mostFrequentUserChoice = i;
+                }
+            }
+            System.out.println("Найбільш часто вибраний хід вами: " + choices[mostFrequentUserChoice]);
+        }
+    }
+
